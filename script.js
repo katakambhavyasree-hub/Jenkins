fb40d1f2f79a4ed8a147b4650114bba2f75d2811
Bhavya Sree <katakambhavyasree@gmail.com>
2026-05-02 11:05:09 +0530

    Add functions to show message and greet user

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function showMessage() {
+    document.getElementById("message").innerHTML = "Hello! You clicked the button 🎉";
+}
 
+function greetUser() {
+    let name = document.getElementById("name").value;
+
+    if (name === "") {
+        document.getElementById("output").innerHTML = "Please enter your name!";
+    } else {
+        document.getElementById("output").innerHTML = "Hello " + name + "! Welcome 👋";
+    }
+}
